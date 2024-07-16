cbba17554af5d0a42afef5403d89d99ee0f33881
user.name <user.email>
2024-07-15 21:29:53 -0400

    typo

--- lib/src/test/java/org/example/utilities/LoadEnvTest.java
+++ lib/src/test/java/org/example/utilities/LoadEnvTest.java
@@ -4,15 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.assertNotNull; // assertTrue, assertEquals, assertNotNull
 
-import java.util.HashMap;
-
 public class LoadEnvTest {
 
     @Test
     @DisplayName("Validates that Credentials load from .env file")
     void loadEnvTest() {
-        final HashMap credentials = LoadEnv.fromFilePath("example_env.txt");
-        assertNotNull(credentials);
+        java.util.HashMap credentials = LoadEnv.fromFilePath("example_env.txt"); // env belongs in <Package>/src or <Package>/lib of gradle folder
         System.out.println("accessKeyId: " + credentials.get("accessKeyId"));
         System.out.println("secretAccessKey: " + credentials.get("secretAccessKey"));
         assertNotNull(credentials.get("accessKeyId"));
